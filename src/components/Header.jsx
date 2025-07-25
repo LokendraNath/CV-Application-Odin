c853e6028f8f6a3a9d12aef588336a840e31692b
Lokendra Nath <nathdeveloper25@gmail.com>
2025-07-25 21:47:28 +0530

    header style make

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 export function Header() {
   return (
-    <header className="fixed right-0 left-0 bg-blue-500 flex items-center justify-center h-16 px-10 border-b-2">
-      <h1 className="text-3xl text-center text-white">Cv Walla</h1>
+    <header className="fixed right-0 left-0 bg-blue-500 flex items-center justify-between h-16 px-10 shadow-2xs">
+      
+      <h1 className="text-4xl text-center text-white tracking-widest">Resume Walla</h1>
+      <ul className="flex">
+        <li className="text-white tracking-widest ml-5">Pricing</li>
+        <li className="text-white tracking-widest ml-5">More Ai Tools</li>
+        <li className="text-white tracking-widest ml-5">About</li>
+      </ul>
     </header>
   );
 }
