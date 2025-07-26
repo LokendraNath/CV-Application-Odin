9854677d98369f957654726a4a7d6e62975a2fdd
Lokendra Nath <nathdeveloper25@gmail.com>
2025-07-26 13:20:22 +0530

    sidebar changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,21 +2,36 @@ import GeneralInfo from "./Sidebar Component/General";
 import Education from "./Sidebar Component/Education";
 import Experience from "./Sidebar Component/Experience";
 
-export function Sidebar({ setUserData, userData }) {
-  function handleSubmit() {
-    alert("Submit");
-  }
+export function Sidebar({
+  setUserData,
+  userData,
+  deleteEdu,
+  deleteExe,
+  deleteSkill,
+}) {
   return (
-    <aside className="bg-blue-300 overflow-y-scroll px-3 mt-12 h-[650px] py-10">
-      <GeneralInfo setUser={setUserData} />
-      <Education setUser={setUserData} userData={userData} />
-      <Experience setUser={setUserData} userData={userData} />
-      <button
+    <aside className="fixed bg-blue-300 overflow-y-scroll px-3 mt-12 h-[650px] max-h-[100vh] py-10 w-[400px]">
+      <GeneralInfo
+        setUser={setUserData}
+        userData={userData}
+        deleteSkill={deleteSkill}
+      />
+      <Education
+        setUser={setUserData}
+        userData={userData}
+        deleteEdu={deleteEdu}
+      />
+      <Experience
+        setUser={setUserData}
+        userData={userData}
+        deleteExe={deleteExe}
+      />
+      {/* <button
         onClick={handleSubmit}
         className="w-full bg-white py-2 text-2xl rounded-full tracking-widest font-bold"
       >
         Submit
-      </button>
+      </button> */}
     </aside>
   );
 }
