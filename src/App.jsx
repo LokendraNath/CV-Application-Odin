b9bf2b71f5667862467797ea4ffd00362dbfeee9
Lokendra Nath <nathdeveloper25@gmail.com>
2025-07-26 13:19:46 +0530

    resume preview

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,65 @@ import { Sidebar } from "./components/Sidebar";
 
 const App = () => {
   const [personData, setPersonData] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    objective: "",
+    generalData: {
+      name: "John Son",
+      email: "johnSon@gmail.com",
+      phone: "3437334434",
+      objective: "Multi-disciplinary expert with proven success in medicine, engineering, and skilled trades. Adept at adapting technical knowledge across fields to provide comprehensive solutions with precision and efficiency.",
+      skill: ["Doctor", "Plumber","Engeeneer","Pilot"],
+    },
     workExperience: [
       {
-        company: "demoCompnay",
-        compPosition: "Software Dev",
-        start: "2334",
-        end: "343",
+        company: "Microsoft",
+        compPosition: "Senior 'Soft'ware Engeeneer",
+        start: "2001",
+        end: "2005",
       },
     ],
     education: [
       {
-        school: "demoScoool",
-        title: "12th Pass out",
-        date: "2043",
+        school: "Chapra University",
+        title: "Bachler Of Multiple Profession",
+        date: "2020",
       },
     ],
   });
-  console.log(personData.workExperience);
-  console.log(personData.education);
+
+  function deleteSkill(index) {
+    setPersonData((prev) => ({
+      ...prev,
+      generalData: {
+        ...prev.generalData,
+        skill: prev.generalData.skill.filter((_, i) => i !== index),
+      },
+    }));
+  }
+
+  function handleDeleteEdu(index) {
+    const updatedList = personData.education.filter((_, i) => i !== index);
+    setPersonData((prev) => ({
+      ...prev,
+      education: updatedList,
+    }));
+  }
+  function handleDeleteExp(index) {
+    const updatedList = personData.workExperience.filter((_, i) => i !== index);
+    setPersonData((prev) => ({
+      ...prev,
+      workExperience: updatedList,
+    }));
+  }
 
   return (
-    <div className="relative bg-white min-h-screen grid grid-rows-[auto_1fr] grid-cols-[450px_1fr]">
+    <div className=" bg-white min-h-screen grid grid-rows-[auto_1fr] grid-cols-[450px_1fr]">
       <Header />
-      <Sidebar setUserData={setPersonData} userData={personData} />
+      <Sidebar
+        setUserData={setPersonData}
+        userData={personData}
+        deleteEdu={handleDeleteEdu}
+        deleteExe={handleDeleteExp}
+        deleteSkill={deleteSkill}
+      />
       <Content userData={personData} />
     </div>
   );
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,52 +1,71 @@
 export function Content({ userData }) {
   return (
-    <main className="bg-white mt-20 px-10 min-h-screen">
-      <div className="border-2 border-black flex flex-col items-center py-3 px-10 h-full">
-        <h1 className="text-3xl">{userData.name}</h1>
-        <div className="flex gap-5">
-          <div>
-            {userData.email && <i className="fa-solid fa-envelope"></i>}
-            {userData.email}
+    <main className="bg-white mt-20 px-10 min-h-screen absolute right-0 w-[900px] z-0 py-5">
+      
+      <div className="border-2 border-black flex flex-col items-center px-10 h-full py-4">
+        <h1 className="text-3xl font-bold tracking-widest">
+          {userData.generalData.name}
+        </h1>
+        <div className="flex gap-5 mt-3">
+          <div className="flex items-center">
+            {userData.generalData.email && (
+              <i className="fa-solid fa-envelope mr-2"></i>
+            )}
+            <p className="tracking-wider">{userData.generalData.email}</p>
           </div>
-          <div>
-            {userData.phone && <i className="fa-solid fa-phone"></i>}
-            {userData.phone}
+          <div className="flex items-center">
+            {userData.generalData.phone && (
+              <i className="fa-solid fa-phone mr-2"></i>
+            )}
+            <p className="tracking-wider">{userData.generalData.phone}</p>
           </div>
         </div>
-        {userData.objective && (
+        {userData.generalData.objective && (
           <div className="border-b-[1px] h-1 w-[50%] mt-2"></div>
         )}
-        {userData.objective && (
+        {userData.generalData.objective && (
           <div className="mt-10 w-full">
             <h1 className="text-start text-2xl bg-gray-300 inline-block px-3 py-1 rounded-xl">
               Objective:
             </h1>
-            <p className="mt-3 pl-10 text-lg">{userData.objective}</p>
+            <p className="mt-4 pl-10 text-sm font-lugrasimo">
+              " {userData.generalData.objective} "
+            </p>
           </div>
         )}
-        {userData.education && (
+        {userData.generalData.skill.length > 0 && (
+          <div className="mt-10 w-full">
+            <h1 className="text-start text-2xl bg-gray-300 inline-block px-3 py-1 rounded-xl">
+              Skills :
+            </h1>
+            <div className="mt-2 flex w-full pl-10">
+              - &nbsp; <p>{userData.generalData.skill.join(", ")}</p>
+            </div>
+          </div>
+        )}
+        {userData.education.length > 0 && (
           <div className="mt-10 w-full">
             <h1 className="text-start text-2xl bg-gray-300 inline-block px-3 py-1 rounded-xl">
               Education Qualification :
             </h1>
             <ul className="mt-3 pl-10 text-lg">
-              {userData.education.map((e) => (
-                <li>
+              {userData.education.map((e, i) => (
+                <li key={i}>
                   - {e.date} {e.title} {e.school}
                 </li>
               ))}
             </ul>
           </div>
         )}
-        {userData.workExperience && (
-          <div className="mt-10 w-full">
+        {userData.workExperience.length > 0 && (
+          <div className="mt-10 w-full mb-5">
             <h1 className="text-start text-2xl bg-gray-300 inline-block px-3 py-1 rounded-xl">
               Working Experience :
             </h1>
             <ul className="mt-3 pl-10 text-lg">
-              {userData.workExperience.map((e) => (
-                <li>
-                  - {e.start} to {e.end} as a {e.compPosition} at {e.company}
+              {userData.workExperience.map((e, i) => (
+                <li key={i}>
+                  - {e.start} to {e.end} as a {e.compPosition} in {e.company}
                 </li>
               ))}
             </ul>
