dbf42b3d07282d16ab164dca0dcb65db5baa31a2
Lokendra Nath <nathdeveloper25@gmail.com>
2025-07-26 13:20:02 +0530

    header ui change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header className="fixed right-0 left-0 bg-blue-500 flex items-center justify-between h-16 px-10 shadow-2xs">
+    <header className="fixed right-0 left-0 bg-blue-500 flex z-10  items-center justify-between h-16 px-10 shadow-2xs">
       
       <h1 className="text-4xl text-center text-white tracking-widest">Resume Walla</h1>
       <ul className="flex">
